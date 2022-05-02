5b5e278182d9db6a5f1a97d02d4cc4b110b50e79
Allen Ben Philipose <allenalvin333@gmail.com>
2022-05-02 08:51:20 +0530

    N-th Tribonacci Number

--- W1/1/2.py
+++ W1/1/2.py
@@ -6,6 +6,6 @@ class Solution:
         if(n==0): return 0
         if(n==1 or n==2): return 1
         a, b, c = 0, 1, 1
-        for z in range(3,n+1):
+        for z in range(3, n+1):
             a, b, c = b, c, a+b+c
         return c
