a791cc60a00bec8baf046352cc7e57383bac5997
Allen Ben Philipose <allenalvin333@gmail.com>
2022-05-03 08:51:57 +0530

    Maximum Sum Circular Subarray

--- W1/5/2.py
+++ W1/5/2.py
@@ -2,9 +2,11 @@
 # https://leetcode.com/problems/maximum-sum-circular-subarray/
 
 class Solution:
-    def rob(self, nums: List[int]) -> int:
-        a1, b1, a2, b2 = nums[0], 0, 0, 0
-        for z in nums[1:-1]:
-            a1, b1 = z+b1, max(a1, b1)
-            a2, b2 = z+b2, max(a2, b2)
-        return max(a1, b1, nums[-1]+b2, a2)
+    def maxSubarraySumCircular(self, nums: List[int]) -> int:
+        l, ans, h, k = len(nums), nums[0], [(0,-1)], 0
+        for z in range(2*l):
+            k += nums[z%l]
+            while(h and z-h[0][1]>l): heappop(h)            
+            ans = max(ans,k-h[0][0])
+            heappush(h,(k,z))
+        return ans
