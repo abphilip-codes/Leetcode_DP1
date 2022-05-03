4cd0cdd438e83f32233afe5f224e0aa1acd9f280
Allen Ben Philipose <allenalvin333@gmail.com>
2022-05-03 08:30:55 +0530

    Jump Game II

--- W1/4/2.py
+++ W1/4/2.py
@@ -2,9 +2,9 @@
 # https://leetcode.com/problems/jump-game-ii/
 
 class Solution:
-    def rob(self, nums: List[int]) -> int:
-        a1, b1, a2, b2 = nums[0], 0, 0, 0
-        for z in nums[1:-1]:
-            a1, b1 = z+b1, max(a1, b1)
-            a2, b2 = z+b2, max(a2, b2)
-        return max(a1, b1, nums[-1]+b2, a2)
+    def jump(self, nums: List[int]) -> int:
+        ans, m, k = 0, 0, 0
+        for z in range(len(nums)-1):
+            m = max(m, z+nums[z])
+            if(k==z): ans, k = ans+1, m
+        return ans
