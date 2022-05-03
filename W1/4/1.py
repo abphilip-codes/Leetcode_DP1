f4630253ce15c96426fef1274d7648940281e9de
Allen Ben Philipose <allenalvin333@gmail.com>
2022-05-03 08:10:39 +0530

    Jump Game

--- W1/4/1.py
+++ W1/4/1.py
@@ -6,6 +6,6 @@ class Solution:
         n = len(nums)
         a, b = n-2, n-1
         while(a>=0):
-            if(nums[a]+a>=b): b = a
+            if(nums[a]+a>=b): b=a
             a-=1
         return (b==0)
