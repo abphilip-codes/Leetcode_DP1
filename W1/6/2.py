b9fcf4718961328e0a0a6166cc75bce129550ce4
Allen Ben Philipose <allenalvin333@gmail.com>
2022-05-03 10:25:42 +0530

    Completed

--- W1/6/2.py
+++ W1/6/2.py
@@ -5,11 +5,11 @@ class Solution:
     def getMaxLen(self, nums: List[int]) -> int:
         ans, a, b, k = 0, -1, None, 1 
         for z in range(len(nums)):
-            if not nums[z]: a, b, k = z, None, 1
+            if(nums[z]==0): a, b, k = z, None, 1
             else:
-                k *= nums[z]/abs(nums[z])
-                if(k>0): ans = max(ans, z-a)
-                elif(k<0):
-                    if not b: b = z
+                k *= nums[z] / abs(nums[z]) 
+                if(k==1): ans = max(ans, z-a)
+                else:
+                    if b is None: b = z
                     ans = max(ans, z-b)
         return ans
--- W1/6/README.md
+++ W1/6/README.md
@@ -1,1 +1,1 @@
-# Dynamic Programming I: Day 6 
+# Dynamic Programming I: Day 6
