ba929568bc0ff43144166c9ef90f1d39c39b1c03
Allen Ben Philipose <allenalvin333@gmail.com>
2022-05-03 11:12:33 +0530

    Best Sightseeing Pair

--- W1/7/1.py
+++ W1/7/1.py
@@ -2,7 +2,9 @@
 # https://leetcode.com/problems/best-sightseeing-pair/
 
 class Solution:
-    def rob(self, nums: List[int]) -> int:
-        a, s = 0, 0
-        for z in nums: a, s = z+s, max(a, s)
-        return max(a, s)
+    def maxScoreSightseeingPair(self, values: List[int]) -> int:
+        m, ans = values[-1]-len(values), 0
+        for z in range(len(values)-2, -1, -1):
+            m = max(m, values[z+1]-z-1)
+            ans = max(ans, values[z]+z+m)
+        return ans
