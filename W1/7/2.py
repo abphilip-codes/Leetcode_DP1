31f8f25f3c70986433105521fbeb7cf240971910
Allen Ben Philipose <allenalvin333@gmail.com>
2022-05-03 12:07:08 +0530

    Best Time to Buy and Sell Stock

--- W1/7/2.py
+++ W1/7/2.py
@@ -2,9 +2,8 @@
 # https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 
 class Solution:
-    def rob(self, nums: List[int]) -> int:
-        a1, b1, a2, b2 = nums[0], 0, 0, 0
-        for z in nums[1:-1]:
-            a1, b1 = z+b1, max(a1, b1)
-            a2, b2 = z+b2, max(a2, b2)
-        return max(a1, b1, nums[-1]+b2, a2)
+    def maxProfit(self, prices: List[int]) -> int:
+        b, s = -prices[0], 0
+        for z in range(1, len(prices)):
+            b, s = max(b, -prices[z]), max(s, b+prices[z])
+        return s
