e21c06f2bbb446870c28b690e2d09ac5d209b492
Allen Ben Philipose <allenalvin333@gmail.com>
2022-05-03 11:36:50 +0530

    Best Sightseeing Pair

--- W1/7/1.py
+++ W1/7/1.py
@@ -5,7 +5,6 @@ class Solution:
     def maxScoreSightseeingPair(self, values: List[int]) -> int:
         dp = [values[0] + values[1]-1, max(values[1], values[0]-1)]
         for z in range(2, len(values)):
-            dp[1] = dp[1] - 1
-            dp[0] = max(dp[0], values[z] + dp[1])
-            dp[1] = max(dp[1], values[z])
+            dp[0] = max(dp[0], values[z] + dp[1]-1)
+            dp[1] = max(dp[1]-1, values[z])
         return dp[0]
