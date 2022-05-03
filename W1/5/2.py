9a6b901ab05ba048c6bf5650f209c1896f30a620
Allen Ben Philipose <allenalvin333@gmail.com>
2022-05-03 08:59:40 +0530

    Maximum Sum Circular Subarray

--- W1/5/2.py
+++ W1/5/2.py
@@ -3,10 +3,12 @@
 
 class Solution:
     def maxSubarraySumCircular(self, nums: List[int]) -> int:
-        l, ans, h, k = len(nums), nums[0], [(0,-1)], 0
-        for z in range(2*l):
-            k += nums[z%l]
-            while(h and z-h[0][1]>l): heappop(h)            
-            ans = max(ans,k-h[0][0])
-            heappush(h,(k,z))
-        return ans
+        n, s = len(nums), nums[0]
+        c1, c2, g1, g2 = nums[0], nums[0], nums[0], nums[0]
+        for z in range(1, n):
+            c1 = min(nums[z], nums[z]+c1)
+            g1 = min(g1, c1)
+            c2 = max(nums[z], nums[z]+c2)
+            g2 = max(g2, c2)
+            s += nums[z]
+        return g2 if(g2>(s-g1) or not (s-g1)) else (s-g1)
