5b1977025c55ae91c00bd7584386e35614df6b19
Allen Ben Philipose <allenalvin333@gmail.com>
2022-05-05 10:28:43 +0530

    Unique Binary Search Trees

--- W2/4/2.py
+++ W2/4/2.py
@@ -2,12 +2,9 @@
 # https://leetcode.com/problems/unique-binary-search-trees/
 
 class Solution:
-    def numDecodings(self, s: str) -> int:
-        a, b = 1, 0
-        for z in range(len(s)-1, -1, -1):
-            k = s[z]
-            if(k=="0"): a, b = 0, a
-            elif(k=="1"): a, b = a + b, a
-            elif(k=="2" and (z+1<len(s)) and (s[z+1]<"7")): a, b = a + b, a
-            else: b = a
-        return a
+    def numTrees(self, n: int) -> int:        
+        d = {}
+        for z in range(n+1):
+            if(z<=1): d[z] = 1
+            else: d[z] = sum(d[y]*d[z-y-1] for y in range(z))
+        return d[n]
