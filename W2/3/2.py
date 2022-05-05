5041f4625d0b029d1e8cfe74ca1d7276ddac61af
Allen Ben Philipose <allenalvin333@gmail.com>
2022-05-05 10:02:36 +0530

    Decode Ways

--- W2/3/2.py
+++ W2/3/2.py
@@ -1,20 +1,13 @@
-# 42
-# https://leetcode.com/problems/trapping-rain-water/
+# 91
+# https://leetcode.com/problems/decode-ways/
 
 class Solution:
-    def trap(self, height: List[int]) -> int:
-        ans, n = 0, len(height)
-        a = b = 0
-        if(n<=1): return 0
-        while(True):
-            while(b<n and height[b]<=height[a]): b+=1
-            if(b==n): break
-            ans+=(height[a]*(b-a-1)-sum(height[a+1:b]))
-            a = b
-        a, b = n-1, n-2
-        while(True):
-            while(b>=0 and height[b]<height[a]): b-=1
-            if(b==-1): break
-            ans+=(height[a]*(a-b-1)-sum(height[b+1:a]))
-            a, b = b, b-1
-        return ans
+    def numDecodings(self, s: str) -> int:
+        a, b = 1, 0
+        for z in range(len(s)-1, -1, -1):
+            k = s[z]
+            if(k=="0"): a, b = 0, a
+            elif(k=="1"): a, b = a + b, a
+            elif(k=="2" and (z+1<len(s)) and (s[z+1]<"7")): a, b = a + b, a
+            else: b = a
+        return a
