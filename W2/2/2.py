28a1034007aa5c39d45b4b65c6099ed07d354294
Allen Ben Philipose <allenalvin333@gmail.com>
2022-05-05 09:51:11 +0530

    Trapping Rain Water

--- W2/2/2.py
+++ W2/2/2.py
@@ -1,11 +1,20 @@
-# 714
-# https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/
+# 42
+# https://leetcode.com/problems/trapping-rain-water/
 
 class Solution:
-    def maxProfit(self, prices: List[int], fee: int) -> int:
-        b, s = -prices[0], 0
-        for z in range(1, len(prices)):
-            b1, s1 = b, s
-            b = max(b, s1 - prices[z])
-            s = max(s, b1 + prices[z] - fee)
-        return s
+    def trap(self, height: List[int]) -> int:
+        ans, n = 0, len(height)
+        a = b = 0
+        if(n<=1): return 0
+        while(True):
+            while(b<n and height[b]<=height[a]): b+=1
+            if(b==n): break
+            ans+=(height[a]*(b-a-1)-sum(height[a+1:b]))
+            a = b
+        a, b = n-1, n-2
+        while(True):
+            while(b>=0 and height[b]<height[a]): b-=1
+            if(b==-1): break
+            ans+=(height[a]*(a-b-1)-sum(height[b+1:a]))
+            a, b = b, b-1
+        return ans
