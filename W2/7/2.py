f1650145913f2d2fb9a5ef989167ad7ac3f13f7c
Allen Ben Philipose <allenalvin333@gmail.com>
2022-05-07 13:06:06 +0530

    Range Sum Query 2D - Immutable

--- W2/7/2.py
+++ W2/7/2.py
@@ -4,10 +4,16 @@
 class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
-        self.m = [list(accumulate(z))+[0] for z in matrix]
+        m = len(matrix)
+        n = len(matrix[0])
+        if(m==0 or n==0): return
+        self.arr = [[0]*(n+1) for z in range(m+1)]
+        for z in range(1,m+1):
+            for y in range(1,n+1):
+                self.arr[z][y] = self.arr[z][y-1] + self.arr[z-1][y] + matrix[z-1][y-1] - self.arr[z-1][y-1]
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        return sum(self.m[z][col2] - self.m[z][col1-1] for z in range(row1, row2+1))   
+        return self.arr[row2+1][col2+1] - self.arr[row1][col2+1] - self.arr[row2+1][col1] + self.arr[row1][col1]
 
 
 # Your NumMatrix object will be instantiated and called as such:
