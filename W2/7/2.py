7c312f7453f5e2f79c8ee71870d1ad9f610ecc46
Allen Ben Philipose <allenalvin333@gmail.com>
2022-05-07 13:00:34 +0530

    Range Sum Query 2D - Immutable

--- W2/7/2.py
+++ W2/7/2.py
@@ -1,11 +1,15 @@
 # 304
 # https://leetcode.com/problems/range-sum-query-2d-immutable/
 
-class Solution:
-    def getRow(self, rowIndex: int) -> List[int]:
-        ans = []
-        for z in range(rowIndex+1):
-            k = (math.factorial(rowIndex) // 
-                (math.factorial(z) * math.factorial(rowIndex-z)))
-            ans.append(k)
-        return ans
+class NumMatrix:
+
+    def __init__(self, matrix: List[List[int]]):
+        self.m = [list(accumulate(z))+[0] for z in matrix]
+
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+        return sum(self.m[z][col2] - self.m[z][col1-1] for z in range(row1, row2+1))   
+
+
+# Your NumMatrix object will be instantiated and called as such:
+# obj = NumMatrix(matrix)
+# param_1 = obj.sumRegion(row1,col1,row2,col2)
