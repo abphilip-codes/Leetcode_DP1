eea7af4dc71a7c88bb5c537c5aa4defdf1db740a
Allen Ben Philipose <allenalvin333@gmail.com>
2022-05-23 09:48:11 +0530

    Unique Paths

--- W3/1/1.py
+++ W3/1/1.py
@@ -2,22 +2,5 @@
 # https://leetcode.com/problems/unique-paths/
 
 class Solution:
-    def matrixBlockSum(self, mat: List[List[int]], k: int) -> List[List[int]]:
-        m, n = len(mat), len(mat[0])
-        a = [[0]*(n+1) for _ in range(m+1)]
-        for z in range(1, m+1):
-            for y in range(1, n+1):
-                a[z][y] = a[z-1][y] + a[z][y-1] - a[z-1][y-1] + mat[z-1][y-1]
-
-        def f(z1, y1, z2, y2):
-            z1 = max(z1, 1)
-            z2 = min(z2, m)
-            y1 = max(y1, 1)
-            y2 = min(y2, n)
-            return a[z2][y2] - a[z1-1][y2] - a[z2][y1-1] + a[z1-1][y1-1]
-        
-        ans = [[0]*n for _ in range(m)]
-        for z in range(1,m+1):
-            for y in range(1,n+1):
-                ans[z-1][y-1] = f(z-k,y-k,z+k,y+k)
-        return ans
+    def uniquePaths(self, m: int, n: int) -> int:
+        return math.factorial(m+n-2)//(math.factorial(m-1)*math.factorial(n-1))
