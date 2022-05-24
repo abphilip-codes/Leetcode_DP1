0c3ffc07800c8fa5a20ecced272dbc9abeb671d1
Allen Ben Philipose <allenalvin333@gmail.com>
2022-05-24 10:24:35 +0530

    Minimum Path Sum

--- W3/2/1.py
+++ W3/2/1.py
@@ -2,5 +2,14 @@
 # https://leetcode.com/problems/minimum-path-sum/
 
 class Solution:
-    def uniquePaths(self, m: int, n: int) -> int:
-        return math.factorial(m+n-2)//(math.factorial(m-1)*math.factorial(n-1))
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        a, b = len(grid), len(grid[0])
+        arr = [[0]*(b) for z in range(a)]
+        
+        for z in range(a):
+            for y in range(b):
+                k = min(arr[z-1][y] if(z>0) else float('inf'), \
+                        arr[z][y-1] if(y>0) else float('inf'))
+                
+                arr[z][y] = grid[z][y] + (k if k!=float('inf') else 0)
+        return arr[-1][-1]
