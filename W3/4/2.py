4b2ddd0f53377ca4adecdd90036cf4587af59e56
Allen Ben Philipose <allenalvin333@gmail.com>
2022-05-26 12:46:24 +0530

    Wiggle Subsequence

--- W3/4/2.py
+++ W3/4/2.py
@@ -2,17 +2,15 @@
 # https://leetcode.com/problems/wiggle-subsequence/
 
 class Solution:
-    def longestPalindromeSubseq(self, s: str) -> int:
-        if(s==s[::-1]): return len(s)
-        n = len(s)
-        arr = [0 for y in range(n)]
-        arr[n-1] = 1
-
-        for z in range(n-1, -1, -1):
-            t = arr[:]
-            t[z] = 1
-            for y in range(z+1, n):
-                if(s[z]==s[y]): t[y] = 2 + arr[y-1]
-                else: t[y] = max(arr[y], t[y-1])
-            arr = t
-        return arr[n-1]
+    def wiggleMaxLength(self, nums: List[int]) -> int:
+        if not nums: return 0
+        ans, k = 1, None
+        for z in range(len(nums)-1):
+            if(nums[z+1]-nums[z]==0): continue
+            if(nums[z+1]-nums[z]>0):
+                if(k==None or k==False): ans+=1
+                k = True
+            if(nums[z+1]-nums[z]<0):
+                if(k==None or k==True): ans+=1
+                k = False
+        return ans
