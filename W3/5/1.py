dbd57b1000301ff05e94af65e863ae9e671fdaae
Allen Ben Philipose <allenalvin333@gmail.com>
2022-05-27 09:35:40 +0530

    README

---
+++ W3/5/1.py
@@ -0,0 +1,10 @@
+# 392
+# https://leetcode.com/problems/is-subsequence/
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        k = [nums[0]]
+        for z in range(1,len(nums)):
+            if(nums[z] > k[-1]): k.append(nums[z])
+            else: k[bisect_left(k, nums[z], 0, len(k))] = nums[z]
+        return len(k)
---
+++ W3/5/3.py
@@ -0,0 +1,16 @@
+# 72
+# https://leetcode.com/problems/edit-distance/
+
+class Solution:
+    def wiggleMaxLength(self, nums: List[int]) -> int:
+        if not nums: return 0
+        ans, k = 1, None
+        for z in range(len(nums)-1):
+            if(nums[z+1]-nums[z]==0): continue
+            if(nums[z+1]-nums[z]>0):
+                if(k==None or k==False): ans+=1
+                k = True
+            if(nums[z+1]-nums[z]<0):
+                if(k==None or k==True): ans+=1
+                k = False
+        return ans
