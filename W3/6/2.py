b056fdf88257f2bbf146cc700048e2373de478d5
Allen Ben Philipose <allenalvin333@gmail.com>
2022-05-28 08:22:29 +0530

    Coin Change 2

--- W3/6/2.py
+++ W3/6/2.py
@@ -2,14 +2,10 @@
 # https://leetcode.com/problems/coin-change-2/
 
 class Solution:
-    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        t1, k, ans = defaultdict(deque), [], []
-        for y, z in enumerate(text1):
-            if(z in set(text2)): t1[z].appendleft(y)
-        for z in text2:
-            if(z in set(text1)): k += list(t1[z])
-        for z in k:
-            y = bisect_left(ans, z)
-            if(y==len(ans)): ans.append(z)
-            else: ans[y] = z
-        return len(ans)
+    def change(self, amount: int, coins: List[int]) -> int:
+        arr = [0]*(amount+1)
+        arr[0] = 1
+        for z, k in enumerate(coins):
+            for y in range(k, amount+1):
+                arr[y] += arr[y-k]
+        return arr[amount]
