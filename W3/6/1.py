d2d86175364c1f0a4def2c942c3fa4a1bba9da22
Allen Ben Philipose <allenalvin333@gmail.com>
2022-05-28 08:29:02 +0530

    Coin Change

--- W3/6/1.py
+++ W3/6/1.py
@@ -2,9 +2,11 @@
 # https://leetcode.com/problems/coin-change/
 
 class Solution:
-    def isSubsequence(self, s: str, t: str) -> bool:
-        k = -1
-        for z in range(len(s)):
-            k = t.find(s[z], k+1)
-            if(k==-1): return False
-        return True
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        if(amount==0): return 0
+        arr = [0]+[math.inf]*amount
+        coins = sorted(coins)
+        for z in range(1,amount+1):
+            if(z>=coins[0]): 
+                arr[z] = 1 + min(arr[z-y] for y in coins if(y<=z))
+        return arr[-1] if(arr[-1]<=amount) else -1
