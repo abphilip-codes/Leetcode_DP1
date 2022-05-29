49e63e64d27a2a69fe342dcee1d1130a8d794d76
Allen Ben Philipose <allenalvin333@gmail.com>
2022-05-29 07:39:39 +0530

    Combination Sum IV

--- W3/7/1.py
+++ W3/7/1.py
@@ -2,9 +2,15 @@
 # https://leetcode.com/problems/combination-sum-iv/
 
 class Solution:
-    def isSubsequence(self, s: str, t: str) -> bool:
-        k = -1
-        for z in range(len(s)):
-            k = t.find(s[z], k+1)
-            if(k==-1): return False
-        return True
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        def f(nums, t, d, ans=0):
+            if(t in d): return d[t]
+            if(t==0): return 1
+            if(t<0): return
+            for z in nums:
+                if(t>=z): ans+=f(nums, t-z, d)
+                else: break
+            d[t] = ans
+            return ans
+        return f(nums, target, {})
