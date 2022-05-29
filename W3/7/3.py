3d4b4dc01261c4cc7b9f5bd2695bc5378fca22a8
Allen Ben Philipose <allenalvin333@gmail.com>
2022-05-29 07:42:34 +0530

    Perfect Squares

--- W3/7/3.py
+++ W3/7/3.py
@@ -2,15 +2,10 @@
 # https://leetcode.com/problems/perfect-squares/
 
 class Solution:
-    def minDistance(self, word1: str, word2: str) -> int:
-        def f(word1, z, word2, y, c):
-            if(z<0 or y<0): return z + 1 + y + 1
-            if((z, y) in c): return c[(z, y)]
-
-            if(word1[z]==word2[y]): result = f(word1, z-1, word2, y-1, c)
-            else: result = min(f(word1, z-1, word2, y, c),
-                               f(word1, z, word2, y-1, c),
-                               f(word1, z-1, word2, y-1, c)) + 1
-            c[(z, y)] = result
-            return result
-        return f(word1, len(word1)-1, word2, len(word2)-1, {})
+    def numSquares(self, n: int) -> int:
+        if(n**(0.5)==int(n**0.5)): return 1
+        while(n&3==0): n>>=2
+        if(n&7==7): return 4
+        for z in range(1, int(n**(0.5))+1):
+            if((n-z*z)**(0.5)==int((n-z*z)**(0.5))): return 2
+        return 3
